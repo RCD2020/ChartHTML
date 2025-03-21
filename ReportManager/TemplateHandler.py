d6e9e49446461cb4bb22703270211e83c20a9f67
RCD2020 <dobertravis@icloud.com>
2025-03-20 18:28:44 -0600

    added function to write template to file

--- ReportManager/TemplateHandler.py
+++ ReportManager/TemplateHandler.py
@@ -1,4 +1,31 @@
+TEMPLATE = """<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>{title}</title>
+
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        
+        <style>
+
+        </style>
+    </head>
+
+    <body>
+        
+    </body>
+</html>"""
+
+
 class TemplateHandler:
+
+    def __init__(self, title='ChartApp'):
+        self.title = title
+
     
-    def __init__():
-        pass
+    def write_template(self, path):
+
+        text = TEMPLATE.format(title=self.title)
+
+        with open(path, 'w') as f:
+            f.write(text)
---
+++ main.py
@@ -0,0 +1,5 @@
+from ReportManager.TemplateHandler import TemplateHandler
+
+
+test = TemplateHandler()
+test.write_template('out.html')
